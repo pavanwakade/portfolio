8c3f0ffbb3fcef6e0590438e07260222ce094437
pavanwakade <pavanwakade199@gmail.com>
2025-11-10 21:34:43 +0530

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = ({ darkMode, setDarkMode }) => {
   return (
     <>
       <nav
-        className={`fixed w-full z-50 transition-all duration-500 bg-transparent pointer-events-none`}
+        className={`fixed w-full z-50 transition-all duration-500 bg-transparent pointer-events-none z-50`}
       >
         <div className="max-w-7xl mx-auto px-6 lg:px-8 pointer-events-auto">
           <div className="flex justify-between items-center h-20">
